d35dc9d8389faefe99324188627d25c3f12326e5
Mustafa <mmustafa@jazeeratech.in>
2020-08-31 15:43:21 +0530

    relationships added b/w books and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,24 +8,35 @@ const {
     GraphQLID,
     GraphQLInt } = graphql;
 
-const books = [
-    {id: '1', name: 'Book 1', genre: 'romance'},
-    {id: '2', name: 'Book 2', genre: 'crime'},
-    {id: '3', name: 'Book 3', genre: 'autobiography'}
-]
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
 
-const authors = [
-    {id: '1', name: 'Patrick Rothfuss', age: 45},
-    {id: '2', name: 'Brandon Sanderson', age: 55},
-    {id: '3', name: 'Terry Pratchett', age: 66}
-]
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {type: GraphQLID},
         name: { type: GraphQLString},
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve( parent, args ){
+                console.log("parent parameter is: ", parent);
+                return _.find(authors, {id: parent.authorId});
+            } 
+        }
     })
 })
 
@@ -34,7 +45,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: { type: GraphQLString},
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        books: {
+            type: graphql.GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, {authorId: parent.id})
+            }
+        }
     })
 })
 
