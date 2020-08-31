ebd9e05928d0a8be92389db8e531e1c4221692f8
Mustafa <mmustafa@jazeeratech.in>
2020-08-31 18:56:14 +0530

    added validation for book and author schema types in graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,7 +10,9 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLList,
-    GraphQLInt } = graphql;
+    GraphQLInt,
+    GraphQLNonNull
+ } = graphql;
 
 // dummy data
 /* var books = [
@@ -107,8 +109,8 @@ const Mutation =   new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: GraphQLString},
-                age: { type: GraphQLInt}
+                name: { type: new GraphQLNonNull( GraphQLString ) },
+                age: { type: new GraphQLNonNull( GraphQLInt ) }
             },
             resolve( parent , args){
                 let author = new Author({
@@ -122,9 +124,9 @@ const Mutation =   new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: {type: GraphQLString },
-                genre: { type: GraphQLString },
-                authorId: { type: GraphQLID }
+                name: {type: new GraphQLNonNull( GraphQLString ) },
+                genre: { type: new GraphQLNonNull( GraphQLString ) },
+                authorId: { type: new GraphQLNonNull( GraphQLID ) }
             },
             resolve( parent, args){
                 let book = new Book({
