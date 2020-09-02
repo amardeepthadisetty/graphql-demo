521ad15193a82321abedab8d7899fc8910a767b7
Mustafa <mmustafa@jazeeratech.in>
2020-09-02 15:33:49 +0530

    css styles added, project completed

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -5,18 +5,41 @@ import { getBookQuery } from '../queiries/queiries';
 
 
 class BookDetails extends Component {
-    constructor(props){
-        super(props);
+  
 
+  displayBookDetails() {
+    const { book } = this.props.data;
+
+    if(book){
+      return (
+          <div>
+            <h1>{book.name}</h1>
+            <p>{book.genre}</p>
+            <p>{book.author.name} </p>
+            <p>All Books by this author</p>
+            <ul className='other-books'>
+                {
+                  book.author.books.map(book => {
+                    return (
+                    <li key={book.id}>{book.name}</li>
+                    )
+                  })
+                }
+            </ul>
+          </div>
+      )
+    }else{
+      return ( <div>No Book selected...!</div>)
     }
-    
+  }
 
   render(){
-      console.log(" propsssss is: ", this.props);
+
+    
     return (
       <div id="book-details">
         <p>Output Book details here</p>
-        { this.props.data.book }
+        { this.displayBookDetails() }
       </div>
     );
 
